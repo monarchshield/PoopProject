156986c4a7f7bd96b7aeb6bd86c1d5e50daae452
Anthony Bogli <anthonybogli1@gmail.com>
2015-09-23 16:56:44 +1000

    Just started a grey box scene
    TODO: Add stuff

---
+++ ProjectPoop/Assets/Scripts/BaseCharacter.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BaseCharacter : MonoBehaviour {
+
+	public float _HungerGauge;
+	public float _CrapGauge;
+	public int _Money;
+
+	public int _CrapIncriment;
+	public int _HungerIncriment;
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+	
+	}
+
+
+}
---
+++ ProjectPoop/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,73 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+	Vector3 m_Position;
+	float m_Rotation; 
+	public float m_speed;
+	
+	
+	public Rigidbody m_rigidbody;
+	public KeyCode forward 		=  KeyCode.W;
+	public KeyCode backward 	=  KeyCode.S;
+	public KeyCode rotateLeft   =  KeyCode.A;
+	public KeyCode rotateRight  =  KeyCode.D;
+	public float m_jumpCooldown = 0;
+	public KeyCode Jump  		=  KeyCode.Space;
+	
+	// Use this for initialization
+	void Start () 
+	{
+		m_Position = transform.position;
+		m_Rotation = 0.1f;
+		m_speed = 5.0f;
+		m_jumpCooldown = 0;
+		m_rigidbody = GetComponent<Rigidbody>();
+		
+		
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		
+		m_jumpCooldown -= Time.deltaTime;
+		UserInput ();
+	}
+	
+	void UserInput()
+	{
+		
+		if (Input.GetKey (forward)) 
+		{
+			transform.Translate (Vector3.forward * m_speed * Time.deltaTime);
+		}
+		
+		
+		if (Input.GetKey (rotateRight)) 
+		{
+			transform.Rotate (new Vector3 (0, 1, 0), 3);
+		}
+		
+		if (Input.GetKey (rotateLeft)) 
+		{
+			transform.Rotate (new Vector3 (0, 1, 0), -3);
+		}
+		
+		
+		if (Input.GetKey (backward)) 
+		{
+			transform.Translate (Vector3.back * m_speed * Time.deltaTime);
+		}
+		
+		
+		if(Input.GetKey(Jump) && m_jumpCooldown < 0)
+		{
+			
+			m_rigidbody.AddForce(new Vector3(0,300,0));
+			m_jumpCooldown = 1.0f;
+		}
+	}
+}
