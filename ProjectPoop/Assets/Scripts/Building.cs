735fd125cb81e52bb8174d053b2bc944426eb6f7
Anthony Bogli <anthonybogli1@gmail.com>
2015-10-12 12:40:08 +1100

    Commiting more crappy work

---
+++ ProjectPoop/Assets/Scripts/Building.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Building : MonoBehaviour {
+
+    [Range(1,10)]
+    public int _NumofFloors;
+    public GameObject _BottomfloorMesh;
+    public GameObject _MiddlefloorMesh;
+    public GameObject _TopfloorMesh;
+    public GameObject _EmptyChild;
+
+
+    public Vector3 _Position;
+
+
+	// Use this for initialization
+	void Awake () 
+    {
+        _Position = transform.position;
+
+        CreateBuilding();
+	}
+	
+    
+
+    void CreateBuilding()
+    {
+
+       float YaxisIncriment = .08f;
+
+       Instantiate(_BottomfloorMesh, _Position, Quaternion.identity);
+    
+
+        Instantiate(_BottomfloorMesh, _Position, Quaternion.identity);
+         _Position.y += YaxisIncriment;
+
+        for (int i = 0; i < _NumofFloors; i++)
+        {
+            Instantiate(_MiddlefloorMesh, _Position, Quaternion.identity);
+            _Position.y += .05f;
+        }
+
+        Instantiate(_TopfloorMesh, _Position, Quaternion.identity);
+    }
+
+}
